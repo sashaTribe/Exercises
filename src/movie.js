09cc34f24170a7753ab0be27eec027581cf5d256
sashaTribe <sasha555.tribe@gmail.com>
2024-06-06 17:39:04 +0100

    created interface called Movie

---
+++ src/movie.js
@@ -0,0 +1,8 @@
+"use strict";
+let myMovie = {
+    title: "Blade Runner 2049",
+    director: "Dennis Villenevue",
+    releaseYear: 2017,
+    isAvailableOnStreaming: true
+};
+console.log(myMovie);
---
+++ src/movie.ts
@@ -0,0 +1,15 @@
+interface Movie {
+    title: string,
+    director: string,
+    releaseYear: number,
+    isAvailableOnStreaming: boolean
+}
+
+let myMovie = {
+    title: "Blade Runner 2049",
+    director: "Dennis Villenevue",
+    releaseYear: 2017,
+    isAvailableOnStreaming: true
+}
+
+console.log(myMovie);
