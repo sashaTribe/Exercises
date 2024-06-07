553c9399b0193de62aea792117b44845ce5dbf35
sashaTribe <sasha555.tribe@gmail.com>
2024-06-07 09:52:05 +0100

    added commenting for index value

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+// creating a variable of type number
 let num:number = 9;
+// creating a variable of type string
 let bigName:string = "Sasha";
+// creating a variable of type boolean
 let truth:boolean = false;
 
+// printing all my created values into one string 
 console.log(`My chosen number is ${num}, my name is ${bigName}, and the truth is ${truth}`);
 
