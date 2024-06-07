9219938c5797ae7e4a899dabd897cf737435e9ba
sashaTribe <sasha555.tribe@gmail.com>
2024-06-07 10:14:41 +0100

    commented on class file

--- src/course.ts
+++ src/course.ts
@@ -1,12 +1,27 @@
 class Course {
+    /**
+     * This class is the blueprint of a course description
+     * 
+     * Attributes: 
+     *  - title: title of the course (string)
+     *  - duration: number of weeks the course will take (number)
+     */
     title: string;
     duration: number;
 
+    /**
+     * This constructor initialises object with given values
+     * @param title 
+     * @param duration 
+     */
     constructor(title: string, duration:number) {
         this.title = title;
         this.duration = duration;
     }
 
+    /**
+     * This function prints out a description of the course with the name and duration 
+     */
     describe() {
         console.log(`This course is ${this.title} and this will go on for ${this.duration} weeks`);
     
@@ -15,5 +30,7 @@ class Course {
 
 }
 
+// Create Course object called javaCourse
 let javaCourse = new Course("Java", 8);
+// prints "This course is Java and this will go on for 8 weeks"
 javaCourse.describe();
